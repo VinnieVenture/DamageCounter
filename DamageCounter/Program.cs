d71973d7116b534d0e21739c6c329fa28b10b3a8
VinnieVenture <vinventurewow@gmail.com>
2022-12-25 20:50:36 +0100

    damage counter for console

--- DamageCounter/Program.cs
+++ DamageCounter/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DamageCounter;
+
+while (true)
+{
+    SwordDamage swordDamage = new SwordDamage();
+    Random random = new Random();
+    
+    Console.WriteLine("0 - ani magiczny, ani płonący\n1 - magiczny\n2 - płonący\n3magiczny i płonacy\ninne wartości - koniec: ");
+    if (Int32.TryParse(Console.ReadKey(false).KeyChar.ToString(), out int number))
+    {
+        if (number > 3 || number < 0)
+            return;
+
+        for (int i = 0; i < 3; i++) {
+            swordDamage.Roll += random.Next(1, 7);
+        }
+
+        swordDamage.SetMagic(number == 1 || number == 3);
+        swordDamage.SetFlaming(number == 2 || number == 3);
+
+        Console.WriteLine($"\nRzut: {swordDamage.Roll}. Zadane obrażenia: {swordDamage.Damage}.\n");
+    }
+    else
+    {
+        return;
+    }
+}
---
+++ DamageCounter/SwordDamage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamageCounter
+{
+    class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        public decimal MagicMultipier = 1M;
+        public int Flaming_Damage = 0;
+        public int Damage;
+
+        public void CalculateDamage() {
+            Damage = (int)(Roll * MagicMultipier) + BASE_DAMAGE + Flaming_Damage;
+        }
+
+        public void SetMagic(bool isMAgic) {
+            if (isMAgic)
+            {
+                MagicMultipier = 1.75M;
+            }
+            else {
+                MagicMultipier = 1.0M;
+            }
+            CalculateDamage();
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
+    }
+}
